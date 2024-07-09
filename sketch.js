1bad1a5aadb6be985ed9cda1b8bbeb7e97c0de19
BaezRamosRene <103864945+BaezRamosRene@users.noreply.github.com>
2024-07-08 22:02:04 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function preload() {
   }
 
   for (let i = 1; i <= numImagenesFactura; i++) {
-    let path = 'grupofacturas/Facturas' + i + '.png';
+    let path = 'grupofacturas/Facturas1.png';
     let imagen = loadImage(path);
     imagenesFactura.push(imagen);
   }
